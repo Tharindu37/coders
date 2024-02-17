6d208a4d161750dbff0fab6d6225ae11aa2dd672
Tharindu37 <tharindut520@gmail.com>
2024-02-18 03:04:56 +0530

    create login and auth guard

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,14 @@
+import firebase from 'firebase/compat/app';
+import { Social } from './social';
+
 export interface User {
+  createdAt: firebase.firestore.Timestamp;
+  updatedAt: firebase.firestore.Timestamp;
+  userId: string;
+  displayName: string;
+  photoURL: string;
+  bannerURL: string;
+  accounts: Social[];
+  email: string;
+  emailVerified: boolean;
 }
