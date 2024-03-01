c05fea2893b2a01ee14f072f68e9c761c12b7ed1
Tharindu37 <tharindut520@gmail.com>
2024-03-01 18:56:19 +0530

    create basic dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Marks } from 'src/app/model/marks';
+import { AuthService } from 'src/app/service/auth.service';
+import { MarksService } from 'src/app/service/marks.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(
+    private marksService: MarksService,
+    private authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this.getMarks();
+  }
+  marks: Marks | undefined;
 
+  getMarks() {
+    this.authService
+      .getCurrentUser()
+
+      .subscribe((fireUser: any) => {
+        this.marksService.getMarksByUserId(fireUser.uid).subscribe((m) => {
+          this.marks = m[0] as Marks;
+        });
+      });
+  }
 }
